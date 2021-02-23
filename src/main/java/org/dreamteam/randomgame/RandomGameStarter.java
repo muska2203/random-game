9af3891e70b7c120f79c304504591fd96161b170
Dmitry <muska2203@mail.ru>
2021-02-23 23:49:09 +0300

    Добавил вывод окончания игры в консоль.

--- src/main/java/org/dreamteam/randomgame/RandomGameStarter.java
+++ src/main/java/org/dreamteam/randomgame/RandomGameStarter.java
@@ -4,5 +4,6 @@ public class RandomGameStarter {
 
     public static void main(String[] args) {
         System.out.println("Игра началась!!");
+        System.out.println("Игра закончилась, никто не выйграл!");
     }
 }
