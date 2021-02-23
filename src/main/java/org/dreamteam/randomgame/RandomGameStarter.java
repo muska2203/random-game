deeb270ceddfb4dc604500ad5cf1fcb60ddd2f45
_RemiYT_ <ishablinskij@gmail.com>
2021-02-24 00:21:57 +0300

    Написал тестовый жизненный цикл игры.

--- src/main/java/org/dreamteam/randomgame/RandomGameStarter.java
+++ src/main/java/org/dreamteam/randomgame/RandomGameStarter.java
@@ -1,16 +1,36 @@
 package org.dreamteam.randomgame;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 public class RandomGameStarter {
 
     public static final int MAX_GAME_POINTS = 5;
 
-    public static void main(String[] args) {
-        System.out.println("Игра началась!! Для победы нужно набрать " + MAX_GAME_POINTS + " очков");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public static int pointOfComputer = 0;
+    public static int pointOfPlayer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        boolean gameIsRunning = true;
+        while (gameIsRunning) {
+            Random rand = new Random();
+            int numberOfComputer = rand.nextInt(10);
+            numberOfComputer += 1;
+            System.out.println(numberOfComputer);
+            int numberOfPlayer = Integer.parseInt(in.readLine());
+            if (numberOfPlayer == numberOfComputer) {
+                System.out.println("Молодец, у тебя получилось!");
+                pointOfPlayer = pointOfPlayer + 1;
+                System.out.println("Очко игроку " + pointOfPlayer);
+            } else {
+                pointOfComputer = pointOfComputer + 1;
+                System.out.println("Попробуй ещё раз");
+                System.out.println("Очко компьютеру " + pointOfComputer);
+            }
+            gameIsRunning = pointOfComputer != MAX_GAME_POINTS && pointOfPlayer != MAX_GAME_POINTS;
         }
-        System.out.println("Игра закончилась, никто не выйграл!");
     }
 }
