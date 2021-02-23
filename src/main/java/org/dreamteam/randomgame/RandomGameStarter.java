b8995921a561657da4de9f2eab8e08aeb6e19e69
_RemiYT_ <ishablinskij@gmail.com>
2021-02-23 23:55:33 +0300

    Добавил цель игры

--- src/main/java/org/dreamteam/randomgame/RandomGameStarter.java
+++ src/main/java/org/dreamteam/randomgame/RandomGameStarter.java
@@ -2,8 +2,10 @@ package org.dreamteam.randomgame;
 
 public class RandomGameStarter {
 
+    public static final int MAX_GAME_POINTS = 5;
+
     public static void main(String[] args) {
-        System.out.println("Игра началась!!");
+        System.out.println("Игра началась!! Для победы нужно набрать " + MAX_GAME_POINTS + " очков");
         System.out.println("Игра закончилась, никто не выйграл!");
     }
 }
