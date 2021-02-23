6da228f25c757faedde948ce9af45480ab2b1463
Dmitry <muska2203@mail.ru>
2021-02-23 23:52:23 +0300

    Добавил задержку перед окончанием игры.

--- src/main/java/org/dreamteam/randomgame/RandomGameStarter.java
+++ src/main/java/org/dreamteam/randomgame/RandomGameStarter.java
@@ -4,6 +4,11 @@ public class RandomGameStarter {
 
     public static void main(String[] args) {
         System.out.println("Игра началась!!");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("Игра закончилась, никто не выйграл!");
     }
 }
